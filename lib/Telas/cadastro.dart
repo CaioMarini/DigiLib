dbd5456d118cac082c88686b98fa18b299c1bd05
CaioMarini <caioferrazmarini123@gmail.com>
2020-05-24 13:59:58 -0300

    Alteração Tela Cadastro

--- lib/Telas/cadastro.dart
+++ lib/Telas/cadastro.dart
@@ -1,128 +1,111 @@
 import 'package:flutter/material.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
-
-
 void main() => runApp(Cadastro());
+
 class Cadastro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        
-        appBar: AppBar(
-          title: Text("DigLib"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.book,
-                color: Colors.white,
-                
-              ),
-              onPressed: () {
-                // do something
-              },
-            )
-          ],
-          
-        ),
         body: Container(
-          padding: EdgeInsets.symmetric( horizontal: 100),
+          padding: EdgeInsets.symmetric(horizontal: 70),
           decoration: BoxDecoration(
-            
             image: DecorationImage(
               image: AssetImage("assets/imagens/biblioteca.jpg"),
-              
               fit: BoxFit.cover,
             ),
             color: Colors.white,
-            
           ),
-          
-          
           child: Center(
             child: SingleChildScrollView(
-              child: Column(      
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,       
-                children: [
-                SizedBox(height: 50),
-                Text("DigLib"),
-                Image.asset('assets/imagens/backless.png', scale: 4),
-                SizedBox(height: 50),
-                TextField(
-                decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 50.0),
-                    prefixIcon: Icon(Icons.email),
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Email'
+                child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 2),
+                Text(
+                  "DigiLib",
+                  style: TextStyle(
+                    color: Colors.black,
+                    height: 1.5,
+                    fontSize: 90,
+                    fontWeight: FontWeight.bold,
                   ),
+                ),
+                Image.asset('assets/imagens/backless.png', scale: 1.859),
+                SizedBox(height: 55),
+                TextField(
+                  decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 5.0),
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Email'),
                 ),
                 SizedBox(height: 20),
                 TextField(
                   decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 50.0),
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Usuário'
-                  ),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 50.0),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Usuário'),
                 ),
                 SizedBox(height: 20),
                 TextField(
                   obscureText: true,
                   decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 50.0),
-                    prefixIcon: Icon(Icons.lock),
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Senha'
-                  ),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 50.0),
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Senha'),
                 ),
                 SizedBox(height: 20),
                 ButtonTheme(
                   minWidth: 200,
-                  height: 30,
+                  height: 40,
                   child: RaisedButton(
                     color: Colors.white,
-                    child: Text('Cadastrar'),
+                    child: Text('Cadastrar',
+                    style: TextStyle(height: 1.5, fontSize: 22),),
                     onPressed: () {
                       Alert(
                         context: context,
                         title: "Alerta!!",
                         desc: "Cadastro suspenso, indo para tela inicial!",
-                        image: Image.asset('assets/imagens/backless.png', scale: 4),
+                        image: Image.asset('assets/imagens/backless.png',
+                            scale: 4),
                         buttons: [
                           DialogButton(
                             child: Text(
                               "OK",
-                              style: TextStyle(color: Colors.white, fontSize: 20),
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
                             ),
-                            onPressed: () {                            
-                              
+                            onPressed: () {
+                              Navigator.pop(context);
                               Navigator.pushNamed(context, "/telaInicial");
-                              
                             },
                             width: 120,
                           )
                         ],
                       ).show();
-                      
                     },
                   ),
                 ),
-                
               ],
-              )
-            ),
-            
-          ),     
-        ),    
+            )),
+          ),
+        ),
       ),
     );
   }
