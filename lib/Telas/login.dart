c314347b23b6b51ad124cc83eb9fffa84e3217d9
CaioMarini <caioferrazmarini123@gmail.com>
2020-05-24 13:39:45 -0300

    Alteração Tella Login

--- lib/Telas/login.dart
+++ lib/Telas/login.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-// Change this to fit the PDF file you are using to test.
-const String _documentPath = 'assets/imagens/alice_no_pais_das_maravihas.pdf';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Login());
 
@@ -11,86 +9,107 @@ class Login extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("DigLib"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.book,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                // do something
-              },
-            )
-          ],
-        ),
         body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 100),
+          padding: EdgeInsets.symmetric(horizontal: 70),
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/imagens/background.png"),
+              image: AssetImage("assets/imagens/biblioteca.jpg"),
               fit: BoxFit.cover,
             ),
             color: Colors.white,
           ),
           child: Center(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 50),
-              Text("DigLib"),
-              Image.asset(
-                'assets/imagens/backless.png',
-                scale: 4.0,
-              ),
-              SizedBox(height: 50),
-              TextField(
-                decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.symmetric(horizontal: 50.0),
-                    prefixIcon: Icon(Icons.person),
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Login'),
-              ),
-              SizedBox(height: 50),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.symmetric(horizontal: 50.0),
-                    prefixIcon: Icon(Icons.lock),
-                    border: OutlineInputBorder(),
-                    fillColor: Colors.white,
-                    filled: true,
-                    hintText: 'Password'),
-              ),
-              SizedBox(height: 100),
-              ButtonTheme(
-                minWidth: 200.0,
-                height: 30.0,
-                child: RaisedButton(
-                  color: Colors.white,
-                  child: Text('Login'),
-                  onPressed: () {
-                    print("salve");
-                    Navigator.pushNamed(context, "/telaInicial");
-                  },
+            child: SingleChildScrollView(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 2),
+                Text(
+                  "DigiLib",
+                  style: TextStyle(
+                    color: Colors.black,
+                    height: 1.5,
+                    fontSize: 90,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              ButtonTheme(
-                minWidth: 200.0,
-                height: 30.0,
-                child: RaisedButton(
-                  color: Colors.white,
-                  child: Text('Create'),
-                  onPressed: () {},
+                Image.asset('assets/imagens/backless.png', scale: 1.859),
+                SizedBox(height: 55),
+                TextField(
+                  decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 5.0),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Login'),
                 ),
-              ),
-            ],
-          )),
+                SizedBox(height: 20),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 50.0),
+                      prefixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(),
+                      fillColor: Colors.white,
+                      filled: true,
+                      hintText: 'Password'),
+                ),
+                SizedBox(height: 20),
+                ButtonTheme(
+                  minWidth: 200.0,
+                  height: 40.0,
+                  child: RaisedButton(
+                    color: Colors.white,
+                    child: Text(
+                      'Login',
+                      style: TextStyle(height: 1.5, fontSize: 22),
+                    ),
+                    onPressed: () {
+                      Alert(
+                        context: context,
+                        title: "Alerta!!",
+                        desc: "Login suspenso, indo para tela inicial!",
+                        image: Image.asset('assets/imagens/backless.png',
+                            scale: 4),
+                        buttons: [
+                          DialogButton(
+                            child: Text(
+                              "OK",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              Navigator.pushNamed(context, "/telaInicial");
+                            },
+                            width: 120,
+                          )
+                        ],
+                      ).show();
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                ButtonTheme(
+                  minWidth: 200.0,
+                  height: 40.0,
+                  child: RaisedButton(
+                    color: Colors.white,
+                    child: Text(
+                      'Cadastrar',
+                      style: TextStyle(height: 1.5, fontSize: 22),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/cadastro");
+                    },
+                  ),
+                ),
+              ],
+            )),
+          ),
         ),
       ),
     );
